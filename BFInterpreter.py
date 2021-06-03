09009d5946a9db8e2f31555579d6dc0c2085256f
mfpankau <74994673+mfpankau@users.noreply.github.com>
2021-06-02 21:26:20 -0400

    Update BFInterpreter.py

--- BFInterpreter.py
+++ BFInterpreter.py
@@ -1,5 +1,7 @@
-cells = [0]
-curPos = 0
+
+loopStartPos = 0
+loopEndPos = 0
+
 
 def filter(source):
     chars = []
@@ -10,13 +12,40 @@ def filter(source):
 
 
 def execute():
-    code = filter('jugwengbs.wrb,rbs[]<fsebg>')
-    for cmd in code:
+    code = filter('++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.')
+    output = ''
+    cells = [0]
+    curPos = 0
+    x = 0
+    global loopStartPos
+    global loopEndPos
+    while x < len(code):
+        cmd = code[x]
         if cmd == '<': 
             curPos = 0 if curPos <= 0 else curPos - 1
         if cmd == '>': 
             curPos = curPos + 1
             if curPos >= len(cells): cells.append(0)
+        if cmd == '+':
+            cells[curPos] = cells[curPos] + 1
+        if cmd == '-':
+            cells[curPos] = cells[curPos] - 1
+        if cmd == '[':
+            loopStartPos = x
+        if cmd == ']':
+            if cells[curPos] <= 0: 
+                x = x + 1
+                continue
+            loopEndPos = x
+            x = loopStartPos
+        if cmd == '.':
+            output = output + chr(cells[curPos])
+        if cmd == '.':
+            #get input char
+            pass
+        x = x + 1
+    print(output)
+
 
 if __name__ == '__main__':
     execute()
